7eaca3af245b5d4fa1cc92b14811720e86c96182
tinayetsina <tinayetsina@outlook.com>
2025-01-22 00:19:24 +0100

    update - initialized SDL

--- app/main.c
+++ app/main.c
@@ -2,47 +2,67 @@
 #include <stdio.h>
 #include "extra_file.h"
 #include <stdbool.h>
+#include <stdlib.h>
 
 
 int main(int argv, char** args)
 {
+    (void)argv;
+    (void)args;
+
     print_from_lib();
 
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-
-    bool isRunning = 1;
-    SDL_Event event;
-
-    while (isRunning)
+    //////////////////////////////////
+    // Initialize the SDL Subsystem //
+    //////////////////////////////////
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-            case SDL_QUIT:
-                isRunning = 0;
-                break;
-
-            case SDL_KEYDOWN:
-                if (event.key.keysym.sym == SDLK_ESCAPE)
-                {
-                    isRunning = 0;
-                }
-            }
-        }
-
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-
-        SDL_RenderPresent(renderer);
+        SDL_Log("Error ---> %s",SDL_GetError());
+        return EXIT_FAILURE;
     }
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    /////////////////////////////
+    // Create a new SDL Window //
+    /////////////////////////////
+    SDL_Window *window = SDL_CreateWindow(
+        "SuperMega",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        800,
+        SDL_WINDOW_SHOWN
+    );
 
-    return 0;
+    if (!window)
+    {
+        SDL_Log("Error ---> %s",SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    ///////////////
+    // Main Loop //
+    ///////////////
+    bool quit = false;
+
+    while (!quit)
+    {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+            
+        }
+        
+    }
+
+    //////////////////////
+    // Cleanup and Exit //
+    //////////////////////
+    SDL_DestroyWindow(window);
+    SDL_Quit(); 
+    return EXIT_SUCCESS;
 }
